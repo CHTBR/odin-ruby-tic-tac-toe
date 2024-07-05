9b224c8f74e08c658c1d6dd299e120cf4285b63c
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-07-05 22:16:41 +0200

    Add custom to_s for properly displaying the grid

--- lib/grid.rb
+++ lib/grid.rb
@@ -10,4 +10,13 @@ class Grid
   def set_cell_value(cell_name, value)
     @grid[cell_name] = value
   end
+
+  def to_s
+    string = "  | 0 | 1 | 2 |\n  | - | - | - |\n"
+    @grid.each do |key, row|
+      string << "#{key} | #{row[0]} | #{row[1]} | #{row[2]} |\n"
+      string << "  | - | - | - |\n"
+    end
+    string
+  end
 end
