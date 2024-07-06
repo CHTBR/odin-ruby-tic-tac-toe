a428914ce675b4c38f69da7dbb98a54f39823633
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-07-06 10:03:34 +0200

    Add method taking input until getting correct one

--- lib/input.rb
+++ lib/input.rb
@@ -3,4 +3,12 @@ class Input
   def initialize(game_grid)
     @game_grid = game_grid
   end
+
+  def get_user_input
+    print "Select your move (RowColumn): "
+    input = gets.chomp.upcase
+    return input if valid_input?(input)
+    puts "Incorrect form of input. Correct example: A1."
+    get_user_input
+  end
 end
