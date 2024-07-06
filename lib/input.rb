c871c366c4ab10853dcc7a89a3333e1ace8a28e0
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-07-06 10:49:04 +0200

    Fix code to fit ruby style guide

--- lib/input.rb
+++ lib/input.rb
@@ -4,25 +4,27 @@ class Input
     @game_grid = game_grid
   end
 
-  def get_user_input
+  def return_user_input
     print "Select your move (RowColumn): "
-    input = get_converted_input()
+    input = return_converted_input
     return input if valid_input?(input)
+
     puts "Incorrect form of input. Correct example: A1."
     get_user_input
   end
 
   private
+
   def valid_input?(input)
-    return @game_grid.grid.any? {|key, value| key == input[0] && value[input[1]]} if input
+    return @game_grid.grid.any? { |key, value| key == input[0] && value[input[1]] } if input
+
     false
   end
 
-  def get_converted_input
+  def return_converted_input
     raw_input = gets.chomp.upcase
-    if raw_input.size == 2
-      return [raw_input[0].to_sym, raw_input[1].to_i]
-    end
+    return [raw_input[0].to_sym, raw_input[1].to_i] if raw_input.size == 2
+
     nil
   end
 end
