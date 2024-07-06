115c4cb4e827deb079ee7d995916ca3a3d4e0c34
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-07-06 17:23:39 +0200

    Fix code to fit ruby style guide

--- lib/round.rb
+++ lib/round.rb
@@ -19,7 +19,7 @@ class Round
   end
 
   def won_rows?
-    @game_grid.grid.any? { |key, value| value.count(@player) == 3}
+    @game_grid.grid.any? { |_key, value| value.count(@player) == 3 }
   end
 
   def won_columns?
@@ -34,7 +34,7 @@ class Round
   def won_diagonals?
     tmp_arr = @game_grid.grid.each_with_object(Array.new(2) { [] }).with_index do |((_key, value), a), i|
       a[0] << value[i]
-      a[1] << value[(i-2).abs]
+      a[1] << value[(i - 2).abs]
     end
     tmp_arr.any? { |array| array.count(@player) == 3 }
   end
