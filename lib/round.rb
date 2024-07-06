4d9163ec86e48e4cd520901add9ea71d683876f9
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-07-06 11:14:39 +0200

    Add initial variables

--- lib/round.rb
+++ lib/round.rb
@@ -1,4 +1,8 @@
 # Class for managing a single round of the game
 class Round
-
+  def initialize(game_grid, input_manager, player)
+    @input_manager = input_manager
+    @game_grid = game_grid
+    @player = player
+  end
 end
