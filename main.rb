c92f0aadc00230f7344a19550c1acd0345103d43
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-07-06 17:46:08 +0200

    Add welcome message

--- main.rb
+++ main.rb
@@ -1,4 +1,18 @@
+require_relative "lib/game"
+
 # Class for starting a game
 class Main
+  def initialize
+    @game_manager = Game.new
+    puts %(
+|X|O| |
+|-|-|-|
+|O|X| | Welcome to Tick-Tack-Toe
+|-|-|-|
+| | |X|
 
+)
+  end
 end
+
+Main.new
