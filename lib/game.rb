ff12bce6e640dded094e1f411805213248a950e3
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-07-06 15:33:17 +0200

    Add game_loop that repeats until a player wins

--- lib/game.rb
+++ lib/game.rb
@@ -10,4 +10,13 @@ class Game
     @round_manager = Round.new(@grid_manager, @input_manager)
     @players = %w[X O]
   end
+
+  def game_loop
+    message = nil
+    until message
+      puts @grid_manager
+      message = @round_manager.play_round(@players[0])
+      @players.reverse!
+    end
+  end
 end
